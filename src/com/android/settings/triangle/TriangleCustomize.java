46651a5db6426040d16743e7803850ecd7b79b0c
lion0738 <lion0738@naver.com>
2013-11-01 18:12:15 +0900

    Settings : Custom Carrier Text <2/2>
    This commit was commited by @ttafluorine in Team Triangles.
    Originally Contributed by : @lion0738
    
    Conflicts:
    	res/values/tta_strings.xml

--- src/com/android/settings/triangle/TriangleCustomize.java
+++ src/com/android/settings/triangle/TriangleCustomize.java
@@ -1,16 +1,78 @@
 package com.android.settings.triangle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
 import android.util.Log;
 import com.android.settings.R;
+import android.content.Context;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.text.Spannable;
+import android.util.AttributeSet;
+import android.widget.EditText;
+import android.provider.Settings;
 
 public class TriangleCustomize extends PreferenceFragment {
+
+    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+    private Preference mCustomLabel;
+    private String mCustomLabelText = null;
+	private Context mContext;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		addPreferencesFromResource(R.xml.triangle_customize);
-		
+
+        mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
 	}
+
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
+    }
+	@Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+            alert.setPositiveButton(getResources().getString(R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.android.settings.LABEL_CHANGED");
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel),
+                    new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 }
